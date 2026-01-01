7e346b7971cc319450bc1381fa449c676695a569
Lucky <sharma0306lucky@gmail.com>
2026-01-01 14:09:43 +0530

    adding  mapbox

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-const Listing = require("../models/listing");
+const Listing = require("../models/listing.js");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 
 
 module.exports.index = async (req, res) => {
@@ -8,80 +8,64 @@ module.exports.index = async (req, res) => {
 };
 
 module.exports.renderNewForm = (req, res) => {
-  res.render("listings/new");
+  res.render("listings/new.ejs");
 };
 
 module.exports.showListing = async (req, res) => {
   let { id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    req.flash("error", "Listing you requested doesn't exist");
-    return res.redirect("/listings");
-  }
   const listing = await Listing.findById(id)
-    .populate({
-      path: "reviews",
-      populate: {
-        path: "author",
-      },
-    }).populate("owner");
+    .populate({ path: "reviews", populate: { path: "author" } })
+    .populate("owner");
   if (!listing) {
-    req.flash("error", "Listing you requested doesn't exist");
+    req.flash("error", "Listing you requested for does not exist!");
     return res.redirect("/listings");
   }
   res.render("listings/show", { listing });
 };
 
-module.exports.createListing = async (req, res, next) => {
-  let url = req.file.path;
-  let filename = req.file.filename;
+module.exports.createListing = async (req, res) => {
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
-  newListing.image = { url, filename };
+
+  if (req.file) {
+    const { path: url, filename } = req.file;
+    newListing.image = { url, filename };
+  }
+
   await newListing.save();
-  req.flash("success", "new listing created successfully");
+  req.flash("success", "New listing created!");
   res.redirect("/listings");
 };
 
 module.exports.renderEditForm = async (req, res) => {
   let { id } = req.params;
-  const listing = await Listing.findById(id);
+  let listing = await Listing.findById(id);
   if (!listing) {
-    req.flash("error", "Listing you requested doesn't exist");
+    req.flash("error", "Listing you requested for does not exists!");
     return res.redirect("/listings");
   }
-let originalImageUrl = listing.image.url;
-originalImageUrl = originalImageUrl.replace("/upload", "/upload/h_300,w_250");
-  res.render("listings/edit.ejs", { listing, originalImageUrl });
+  let originalImageUrl = listing.image.url;
+  originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
+  res.render("listings/edit", { listing, originalImageUrl });
 };
 
-module.exports.updateListings = async (req, res) => {
-  const { id } = req.params;
-
-  const listing = await Listing.findById(id);
-  if (!listing) {
-    req.flash("error", "Listing not found");
-    return res.redirect("/listings");
-  }
-  if (!listing.owner || !listing.owner.equals(req.user._id)) {
-    req.flash("error", "You don't have permission to edit");
-    return res.redirect(`/listings/${id}`);
-  }
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  if (req.file) {
-    const { path: url, filename } = req.file;
+module.exports.updateListing = async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  if (typeof req.file !== "undefined") {
+    let url = req.file.path;
+    let filename = req.file.filename;
     listing.image = { url, filename };
     await listing.save();
   }
-
-  req.flash("success", "Listing updated successfully");
+  req.flash("success", "Listing updated");
   res.redirect(`/listings/${id}`);
 };
-
 
 module.exports.deleteListing = async (req, res) => {
   let { id } = req.params;
   let deletedListing = await Listing.findByIdAndDelete(id);
   console.log(deletedListing);
-  req.flash("success", "listing deleted successfully");
+  req.flash("success", "Listing Deleted");
   res.redirect("/listings");
 };
--- models/listing.js
+++ models/listing.js
@@ -4,28 +4,28 @@ const Schema = mongoose.Schema;
 const Review = require("./review.js");
 
 const listingSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required: true,
-    } ,
-    description:String,
-    
-    image:{
-     url: String,
-     filename : String,
-    },
-    price: Number,
-    location: String,
-    country: String,
-     owner: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref : "User" ,
-    },
-    reviews:[{
-        type: Schema.Types.ObjectId,
-        ref: "Review",
-    },
-    ],
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+
+  image: {
+    url: String,
+    filename: String,
+  },
+  price: Number,
+  location: String,
+  country: String,
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  reviews: [{
+    type: Schema.Types.ObjectId,
+    ref: "Review",
+  },
+  ],
 });
 
 listingSchema.post("findOneAndDelete", async (listing) => {
